d70a7c1efaeab78fb0df5e49e16486291b8691ca
Veldanov <anton.veldanov@gmail.com>
2019-02-08 15:14:33 -0800

    Loops

--- strings/src/practice/loops/ForLoop.java
+++ strings/src/practice/loops/ForLoop.java
@@ -3,12 +3,18 @@ package practice.loops;
 public class ForLoop {
 
 	public static void main(String[] args) {
+int counter = 0;
+		for (int idx = 0; idx < 5; idx++) {
+			for(int j=0;j<10;j++) {
+				for(int i=0;i<15;i++) {
+					
+					System.out.println("idx = " + idx +" j =  " + j + "  i = "+ i);
 
-		for(int i=0;i<=100;i=i+2) {
-			System.out.println(i);
-			
+					counter++;
+				}
+			}
 		}
-
+		System.out.println(counter);
 	}
 
 }
